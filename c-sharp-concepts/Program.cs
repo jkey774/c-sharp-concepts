5c53e9d45d039dfb023a90e9591f72d245c81371
Joseph Key <jkey3@brookdale.com>
2021-01-03 15:40:36 -0600

    init

--- c-sharp-concepts/Program.cs
+++ c-sharp-concepts/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using c_sharp_concepts.EventsAndDelegates;
 
 namespace c_sharp_concepts
 {
@@ -6,7 +6,19 @@ namespace c_sharp_concepts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /** Begin Events and Delegates Demo */
+
+            var video = new Video() { Title = "Video1" };
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService(); // subscriber
+            var messageService = new MessageService(); // subscriber
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+
+            /* End Events and Delegates Demo */
         }
     }
 }
